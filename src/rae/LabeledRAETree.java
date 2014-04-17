b719168900fd35e735beb43f725136e304a5f67c
gwenzek <guillaume.wenzek@polytechnique.com>
2014-04-17 17:42:04 +0200

    push

--- src/rae/LabeledRAETree.java
+++ src/rae/LabeledRAETree.java
@@ -12,20 +12,20 @@ public class LabeledRAETree implements LabeledDatum<Double, Integer> {
     RAENode[] T;
     double[] feature;
     Structure structure;
-    int SentenceLength, TreeSize, Label;
-    double TotalScore;
+    int sentenceLength, treeSize, Label;
+    double totalScore;
 
     public LabeledRAETree(int SentenceLength, int Label) {
-        this.SentenceLength = SentenceLength;
-        TreeSize = 2 * SentenceLength - 1;
-        T = new RAENode[TreeSize];
-        structure = new Structure(TreeSize);
+        this.sentenceLength = SentenceLength;
+        treeSize = 2 * SentenceLength - 1;
+        T = new RAENode[treeSize];
+        structure = new Structure(treeSize);
         this.Label = Label;
     }
 
     public LabeledRAETree(int sentenceLength, int Label, int hiddenSize, DoubleMatrix wordsEmbedded) {
         this(sentenceLength, Label);
-        for (int i = 0; i < TreeSize; i++) {
+        for (int i = 0; i < treeSize; i++) {
             T[i] = new RAENode(i, sentenceLength, hiddenSize, wordsEmbedded);
             structure.add(new Pair<Integer, Integer>(-1, -1));
         }
@@ -37,17 +37,17 @@ public class LabeledRAETree implements LabeledDatum<Double, Integer> {
 
     public LabeledRAETree(int SentenceLength, int Label, int HiddenSize, int CatSize, DoubleMatrix WordsEmbedded) {
         this(SentenceLength, Label);
-        for (int i = 0; i < TreeSize; i++) {
+        for (int i = 0; i < treeSize; i++) {
             T[i] = new RAENode(i, SentenceLength, HiddenSize, CatSize, WordsEmbedded);
             structure.add(new Pair<Integer, Integer>(-1, -1));
         }
     }
 
     public int[] getStructureString() {
-        int[] parents = new int[TreeSize];
+        int[] parents = new int[treeSize];
         Arrays.fill(parents, -1);
 
-        for (int i = TreeSize - 1; i >= 0; i--) {
+        for (int i = treeSize - 1; i >= 0; i--) {
             int leftChild = structure.get(i).getFirst();
             int rightChild = structure.get(i).getSecond();
             if (leftChild != -1 && rightChild != -1) {
@@ -78,17 +78,17 @@ public class LabeledRAETree implements LabeledDatum<Double, Integer> {
 
         int HiddenSize = T[0].features.rows;
         feature = new double[HiddenSize * 2];
-        DoubleMatrix tf = new DoubleMatrix(HiddenSize, TreeSize);
-        if (SentenceLength > 1) {
-            for (int i = 0; i < TreeSize; i++)
+        DoubleMatrix tf = new DoubleMatrix(HiddenSize, treeSize);
+        if (sentenceLength > 1) {
+            for (int i = 0; i < treeSize; i++)
                 tf.putColumn(i, T[i].features);
-            tf.muli(1.0 / TreeSize);
+            tf.muli(1.0 / treeSize);
 
-            System.arraycopy(T[2 * SentenceLength - 2].features.data, 0, feature, 0, HiddenSize);
+            System.arraycopy(T[2 * sentenceLength - 2].features.data, 0, feature, 0, HiddenSize);
             System.arraycopy(tf.rowSums().data, 0, feature, HiddenSize, HiddenSize);
         } else {
-            System.arraycopy(T[2 * SentenceLength - 2].features.data, 0, feature, 0, HiddenSize);
-            System.arraycopy(T[2 * SentenceLength - 2].features.data, 0, feature, HiddenSize, HiddenSize);
+            System.arraycopy(T[2 * sentenceLength - 2].features.data, 0, feature, 0, HiddenSize);
+            System.arraycopy(T[2 * sentenceLength - 2].features.data, 0, feature, HiddenSize, HiddenSize);
         }
         return feature;
     }
